e7b8456d28fa14714750f9581517669a0c2953f5
ropf <ropf@itu.dk>
2012-04-21 15:49:02 +0200

    oi

---
+++ instasearch/src/org/hackathon/emfsearch/ui/InstaEMFSearchView.java
@@ -0,0 +1,23 @@
+package org.hackathon.emfsearch.ui;
+
+import it.unibz.instasearch.ui.InstaSearchView;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class InstaEMFSearchView extends InstaSearchView {
+
+	public InstaEMFSearchView() {
+		// TODO Auto-generated constructor stub
+	}
+
+//	@Override
+//	public void createPartControl(Composite parent) {
+//		//TODO
+//	}
+//
+//	@Override
+//	public void setFocus() {
+//		//TODO
+//	}
+
+}
