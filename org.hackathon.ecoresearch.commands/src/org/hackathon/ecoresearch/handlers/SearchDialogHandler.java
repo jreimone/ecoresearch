dedad098da0374abe37f44ecbd153c69d6f1f839
Jan Reimann <janreimone@gmail.com>
2012-04-21 12:17:25 +0200

    generation of local index of open GMF editor

--- org.hackathon.ecoresearch.commands/src/org/hackathon/ecoresearch/handlers/SearchDialogHandler.java
+++ org.hackathon.ecoresearch.commands/src/org/hackathon/ecoresearch/handlers/SearchDialogHandler.java
@@ -1,15 +1,47 @@
 package org.hackathon.ecoresearch.handlers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecoretools.diagram.part.EcoreDiagramEditor;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class SearchDialogHandler extends AbstractHandler {
 
+	private Map<EObject, View> localindex = new HashMap<EObject, View>();
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		IEditorPart editor = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getActiveEditor();
+		Object adapter = editor.getAdapter(EcoreDiagramEditor.class);
+		if(adapter != null){
+			EcoreDiagramEditor diagramEditor = (EcoreDiagramEditor) adapter;
+			Diagram diagram = diagramEditor.getDiagram();
+			EObject element = diagram.getElement();
+			EObject model = EcoreUtil.getRootContainer(element, true);
+			createLocalIndex(diagram);
+		}
 		return null;
+	}
+
+	/**
+	 * @param diagram
+	 */
+	@SuppressWarnings("unchecked")
+	private void createLocalIndex(Diagram diagram) {
+		List<View> visibleChildren = (List<View>) diagram.getVisibleChildren();
+		for (View view : visibleChildren) {
+			localindex.put(view.getElement(), view);
+		}
 	}
 
 }
