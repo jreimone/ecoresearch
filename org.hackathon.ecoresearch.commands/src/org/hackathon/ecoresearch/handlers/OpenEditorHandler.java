1613917c43574940755d802e30a369fc562f6dcf
Jan Reimann <janreimone@gmail.com>
2012-04-26 10:53:16 +0200

    renamed and removed commands

--- org.hackathon.ecoresearch.commands/src/org/hackathon/ecoresearch/handlers/OpenEditorHandler.java
+++ org.hackathon.ecoresearch.commands/src/org/hackathon/ecoresearch/handlers/OpenEditorHandler.java
@@ -5,9 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
@@ -24,63 +21,16 @@ import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
-import org.hackathon.ecoresearch.commands.IOpenEditorCommand;
+import org.hackathon.ecoresearch.ui.IOpenEditorHandler;
 
-public class OpenEditorHandler extends AbstractHandler {
+public class OpenEditorHandler implements IOpenEditorHandler {
 
 	private Map<EObject, View> localindex = new HashMap<EObject, View>();
-
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		Object resourceObject = event.getObjectParameterForExecution(IOpenEditorCommand.PARAM_RESOURCE);
-		Object elementObject = event.getObjectParameterForExecution(IOpenEditorCommand.PARAM_ELEMENT);
-		if(resourceObject instanceof Resource && elementObject instanceof EObject){
-			Resource resource = (Resource) resourceObject;
-			EObject element = (EObject) elementObject;
-			URI uri = resource.getURI();
-			if(uri.isFile()){
-				try {
-					String path = uri.path();
-					//					IEditorDescriptor defaultEditor = PlatformUI.getWorkbench().getEditorRegistry().getDefaultEditor(path);
-					//				String id = defaultEditor.getId();
-					IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-					IPath workspacePath = new Path(path);
-					IFile editorFile = ResourcesPlugin.getWorkspace().getRoot().getFile(workspacePath);
-					IEditorPart openEditor = IDE.openEditor(page, editorFile);
-					if(openEditor instanceof EcoreDiagramEditor){
-						// GMF Diagram Editor
-						EcoreDiagramEditor diagramEditor = (EcoreDiagramEditor) openEditor;
-//						diagramEditor.getDiagramGraphicalViewer().
-						View view = (View) element;	
-						Diagram diagram = (Diagram) EcoreUtil.getRootContainer(view, true);
-						
-					} else if (openEditor instanceof EcoreEditor){
-						// Ecore Tree Editor
-						EcoreEditor treeEditor = (EcoreEditor) openEditor;
-						List<EObject> selectedElement = new ArrayList<EObject>();
-						EditingDomain editingDomain = treeEditor.getEditingDomain();
-						ResourceSet resourceSet = editingDomain.getResourceSet();
-						URI resolveUri = EcoreUtil.getURI(element);
-						element = resourceSet.getEObject(resolveUri, true);
-						selectedElement.add(element);
-						IStructuredSelection selection = new StructuredSelection(selectedElement);
-						treeEditor.getViewer().setSelection(selection);
-					}
-				} catch (PartInitException e) {
-					e.printStackTrace();
-				}
-			}
-
-		}
-		return null;
-	}
-
 
 	/**
 	 * @param diagram
@@ -91,6 +41,59 @@ public class OpenEditorHandler extends AbstractHandler {
 		for (View view : visibleChildren) {
 			localindex.put(view.getElement(), view);
 		}
+	}
+
+
+	@Override
+	public IEditorPart openEditorBySearchResultSelection(EObject selectedResult) {
+		if(selectedResult == null){
+			return null;
+		}
+		Resource resource = selectedResult.eResource();
+		URI uri = resource.getURI();
+		if(uri.isPlatformResource()){
+			try {
+				String platformUriString = uri.toString();
+				//					IEditorDescriptor defaultEditor = PlatformUI.getWorkbench().getEditorRegistry().getDefaultEditor(path);
+				//				String id = defaultEditor.getId();
+				uri = resource.getResourceSet().getURIConverter().normalize(uri);
+				if (uri.segmentCount() > 1 && "resource".equals(uri.segment(0))) {
+					StringBuffer platformResourcePath = new StringBuffer();
+					for (int j = 1, size = uri.segmentCount(); j < size; ++j) {
+						platformResourcePath.append('/');
+						platformResourcePath.append(uri.segment(j));
+					}
+					platformUriString = platformResourcePath.toString();
+				} 
+				IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				IPath workspacePath = new Path(platformUriString);
+				IFile editorFile = ResourcesPlugin.getWorkspace().getRoot().getFile(workspacePath);
+				IEditorPart openEditor = IDE.openEditor(page, editorFile);
+				if(openEditor instanceof EcoreDiagramEditor){
+					// GMF Diagram Editor
+					EcoreDiagramEditor diagramEditor = (EcoreDiagramEditor) openEditor;
+					//					diagramEditor.getDiagramGraphicalViewer().
+					View view = (View) selectedResult;	
+					Diagram diagram = (Diagram) EcoreUtil.getRootContainer(view, true);
+
+				} else if (openEditor instanceof EcoreEditor){
+					// Ecore Tree Editor
+					EcoreEditor treeEditor = (EcoreEditor) openEditor;
+					List<EObject> selectedElement = new ArrayList<EObject>();
+					EditingDomain editingDomain = treeEditor.getEditingDomain();
+					ResourceSet resourceSet = editingDomain.getResourceSet();
+					URI resolveUri = EcoreUtil.getURI(selectedResult);
+					selectedResult = resourceSet.getEObject(resolveUri, true);
+					selectedElement.add(selectedResult);
+					IStructuredSelection selection = new StructuredSelection(selectedElement);
+					treeEditor.getViewer().setSelection(selection);
+				}
+				return openEditor;
+			} catch (PartInitException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 
 }
